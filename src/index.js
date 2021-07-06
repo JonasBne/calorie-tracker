142a23bed60e77507d8d90235aa3d0aeb2971094
Jonas Boone <jonas.boone@student.kdg.be>
2021-07-06 19:29:42 +0200

    Added user interface and updated package.json

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 // Import Materialize
 import "materialize-css/dist/css/materialize.min.css";
 
+// Import icons
+import "@fortawesome/fontawesome-free/css/all.css";
+
 // Import custom css
 import "./css/custom.scss";
 
